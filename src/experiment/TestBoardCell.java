b1049b2c92b7058e73f61ba6c9bf4bf8d0bb8591
AJ Mendes <ajmendes17@gmail.com>
2026-03-10 11:36:00 -0600

    init

---
+++ src/experiment/TestBoardCell.java
@@ -0,0 +1,44 @@
+package experiment;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestBoardCell {
+    private int row;
+    private int col;
+    private Set<TestBoardCell> adjacencies = new HashSet<>();
+    private boolean isRoomCell;
+    private boolean occupied;
+
+    // Constructor
+    public TestBoardCell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public void addAdjacency(TestBoardCell cell) {
+        adjacencies.add(cell);
+    }
+
+    // Return empty set
+    public Set<TestBoardCell> getAdjList() {
+        return new HashSet<>(adjacencies);
+    }
+
+    public void setRoom(boolean isRoom) {
+        this.isRoomCell = isRoom;
+    }
+
+    public boolean isRoom() {
+        return isRoomCell;
+    }
+
+    // Empty implementation
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public boolean getOccupied() {
+        return occupied;
+    }
+}
