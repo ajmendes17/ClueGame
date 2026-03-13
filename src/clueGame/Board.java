94bd5a015babefb2a9ddca71e6656bd21f91727e
AJ Mendes <ajmendes17@gmail.com>
2026-03-13 16:18:56 -0600

    Basic setup for classes - Jake

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,22 +1,76 @@
 package clueGame;
 
-public class Board {
-     /*
-       * variable and methods used for singleton pattern
-       */
-     private static Board theInstance = new Board();
-     // constructor is private to ensure only one can be created
-     private Board() {
-            super() ;
-     }
-     // this method returns the only Board
-     public static Board getInstance() {
-            return theInstance;
-     }
-     /*
-      * initialize the board (since we are using singleton pattern)
-      */
-     public void initialize() {
-     }
+import java.util.HashMap;
+import java.util.Map;
 
+public class Board {
+
+	// board structure
+	private BoardCell[][] grid;
+	private int numRows;
+	private int numColumns;
+
+	// config files
+	private String layoutConfigFile;
+	private String setupConfigFile;
+
+	// room storage
+	private Map<Character, Room> roomMap;
+
+	/*
+	 * variable and methods used for singleton pattern
+	 */
+	private static Board theInstance = new Board();
+
+	// constructor is private to ensure only one can be created
+	private Board() {
+		super();
+	}
+
+	// this method returns the only Board
+	public static Board getInstance() {
+		return theInstance;
+	}
+
+	/*
+	 * initialize the board (since we are using singleton pattern)
+	 */
+	public void initialize() {
+		roomMap = new HashMap<>();
+	}
+
+	// set configuration files
+	public void setConfigFiles(String layoutConfigFile, String setupConfigFile) {
+		this.layoutConfigFile = layoutConfigFile;
+		this.setupConfigFile = setupConfigFile;
+	}
+
+	// load setup file
+	public void loadSetupConfig() throws BadConfigFormatException {
+	}
+
+	// load layout file
+	public void loadLayoutConfig() throws BadConfigFormatException {
+	}
+
+	// getters
+	public int getNumRows() {
+		return numRows;
+	}
+
+	public int getNumColumns() {
+		return numColumns;
+	}
+
+	public BoardCell getCell(int row, int col) {
+		return grid[row][col];
+	}
+
+	public Map<Character, Room> getRoomMap() {
+		return roomMap;
+	}
+
+	public Room getRoom(BoardCell cell) {
+		return roomMap.get(cell.getInitial());
+	}
 }
--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -1,5 +1,83 @@
 package clueGame;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BoardCell {
-    
+	private int row;
+	private int col;
+	private char initial;
+	private DoorDirection doorDirection;
+	private boolean roomLabel;
+	private boolean roomCenter;
+	private char secretPassage;
+	private Set<BoardCell> adjList;
+
+	public BoardCell(int row, int col) {
+		this.row = row;
+		this.col = col;
+		this.doorDirection = DoorDirection.NONE;
+		this.secretPassage = ' ';
+		this.adjList = new HashSet<>();
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public char getInitial() {
+		return initial;
+	}
+
+	public void setInitial(char initial) {
+		this.initial = initial;
+	}
+
+	public DoorDirection getDoorDirection() {
+		return doorDirection;
+	}
+
+	public void setDoorDirection(DoorDirection doorDirection) {
+		this.doorDirection = doorDirection;
+	}
+
+	public boolean isDoorway() {
+		return doorDirection != DoorDirection.NONE;
+	}
+
+	public boolean isLabel() {
+		return roomLabel;
+	}
+
+	public void setRoomLabel(boolean roomLabel) {
+		this.roomLabel = roomLabel;
+	}
+
+	public boolean isRoomCenter() {
+		return roomCenter;
+	}
+
+	public void setRoomCenter(boolean roomCenter) {
+		this.roomCenter = roomCenter;
+	}
+
+	public char getSecretPassage() {
+		return secretPassage;
+	}
+
+	public void setSecretPassage(char secretPassage) {
+		this.secretPassage = secretPassage;
+	}
+
+	public void addAdj(BoardCell cell) {
+		adjList.add(cell);
+	}
+
+	public Set<BoardCell> getAdjList() {
+		return adjList;
+	}
 }
