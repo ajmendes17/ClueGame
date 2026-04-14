aba574e92e0018f0dd9b208e6ff710e88ebd0eb1
AJ Mendes <ajmendes17@gmail.com>
2026-04-14 15:13:04 -0600

    next steps are to display the cards and color/format where necessary

--- src/clueGame/KnownCardsPanel.java
+++ src/clueGame/KnownCardsPanel.java
@@ -1,6 +1,11 @@
 package clueGame;
 
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 public class KnownCardsPanel extends JPanel {
 	private JPanel handPanel;
@@ -9,9 +14,27 @@ public class KnownCardsPanel extends JPanel {
 	private JPanel weaponsPanel;
 
 	public KnownCardsPanel() {
+		setLayout(new GridLayout(4, 1));
+
 		handPanel = new JPanel();
 		peoplePanel = new JPanel();
 		roomsPanel = new JPanel();
 		weaponsPanel = new JPanel();
+
+		configureSection(handPanel, "Cards in Hand");
+		configureSection(peoplePanel, "People");
+		configureSection(roomsPanel, "Rooms");
+		configureSection(weaponsPanel, "Weapons");
+
+		add(handPanel);
+		add(peoplePanel);
+		add(roomsPanel);
+		add(weaponsPanel);
+	}
+
+	private void configureSection(JPanel panel, String title) {
+		panel.setLayout(new GridLayout(0, 1));
+		panel.setBorder(new TitledBorder(new EtchedBorder(), title));
+		panel.add(new JLabel(" "));
 	}
 }
