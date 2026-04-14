d0297683be1a1260c27cd2c4df4f528b6d57ce9a
AJ Mendes <ajmendes17@gmail.com>
2026-04-14 14:43:07 -0600

    Added all the buttons, main function, and then shifted the buttons around a little to be more what I wanted

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -1,8 +1,11 @@
 package clueGame;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -12,6 +15,7 @@ import javax.swing.border.TitledBorder;
 public class GameControlPanel extends JPanel {
 	private JButton nextPlayerButton;
 	private JButton accusationButton;
+	private JButton suggestionButton;
 
 	private JTextField currentTurnField;
 	private JTextField dieRollField;
@@ -23,6 +27,7 @@ public class GameControlPanel extends JPanel {
 
 		nextPlayerButton = new JButton("Next player");
 		accusationButton = new JButton("Make accusation");
+		suggestionButton = new JButton("Make suggestion");
 
 		currentTurnField = new JTextField(15);
 		dieRollField = new JTextField(5);
@@ -40,19 +45,23 @@ public class GameControlPanel extends JPanel {
 
 	private JPanel createTurnPanel() {
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(2, 3));
+		panel.setLayout(new BorderLayout());
 
-		panel.add(new JLabel("Whose turn?"));
-		panel.add(new JLabel("Roll"));
-		panel.add(new JLabel(""));
-
-		panel.add(currentTurnField);
-		panel.add(dieRollField);
+		JPanel infoPanel = new JPanel();
+		infoPanel.setLayout(new GridLayout(2, 2));
+		infoPanel.add(new JLabel("Whose turn?"));
+		infoPanel.add(new JLabel("Roll"));
+		infoPanel.add(currentTurnField);
+		infoPanel.add(dieRollField);
 
 		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(3, 1, 0, 5));
 		buttonPanel.add(nextPlayerButton);
 		buttonPanel.add(accusationButton);
-		panel.add(buttonPanel);
+		buttonPanel.add(suggestionButton);
+
+		panel.add(infoPanel, BorderLayout.CENTER);
+		panel.add(buttonPanel, BorderLayout.EAST);
 
 		panel.setBorder(new TitledBorder(new EtchedBorder(), "Game Control"));
 		return panel;
@@ -83,5 +92,19 @@ public class GameControlPanel extends JPanel {
 
 	public void setGuessResult(String guessResult) {
 		guessResultField.setText(guessResult);
+	}
+
+	public static void main(String[] args) {
+		GameControlPanel panel = new GameControlPanel();
+		JFrame frame = new JFrame();
+		frame.setContentPane(panel);
+		frame.setTitle("Game Control Panel");
+		frame.setSize(750, 180);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+		panel.setTurn(new ComputerPlayer("Col. Mustard", Color.ORANGE, 0, 0), 5);
+		panel.setGuess("I have no guess!");
+		panel.setGuessResult("So you have nothing?");
 	}
 }
