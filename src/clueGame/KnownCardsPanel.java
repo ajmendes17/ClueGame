26f244c46638910f08c547126bd5cc027aaa65dc
AJ Mendes <ajmendes17@gmail.com>
2026-04-20 12:16:44 -0600

    added a way to see the human player's hand

--- src/clueGame/KnownCardsPanel.java
+++ src/clueGame/KnownCardsPanel.java
@@ -1,9 +1,15 @@
 package clueGame;
 
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
@@ -32,9 +38,48 @@ public class KnownCardsPanel extends JPanel {
 		add(weaponsPanel);
 	}
 
+	public void setHand(Player humanPlayer) {
+		if (humanPlayer == null) {
+			setHand((Set<Card>) null);
+			return;
+		}
+		setHand(humanPlayer.getHand());
+	}
+
+	public void setHand(Set<Card> cards) {
+		resetSection(handPanel, "Cards in Hand");
+
+		if (cards == null || cards.isEmpty()) {
+			handPanel.add(new JLabel("No cards"));
+		} else {
+			List<Card> sortedCards = new ArrayList<>(cards);
+			sortedCards.sort(Comparator.comparing(Card::getType).thenComparing(Card::getCardName));
+
+			for (Card card : sortedCards) {
+				addCardField(handPanel, card, Color.WHITE);
+			}
+		}
+
+		handPanel.revalidate();
+		handPanel.repaint();
+	}
+
 	private void configureSection(JPanel panel, String title) {
+		resetSection(panel, title);
+		panel.add(new JLabel(" "));
+	}
+
+	private void resetSection(JPanel panel, String title) {
+		panel.removeAll();
 		panel.setLayout(new GridLayout(0, 1));
 		panel.setBorder(new TitledBorder(new EtchedBorder(), title));
-		panel.add(new JLabel(" "));
+	}
+
+	private void addCardField(JPanel panel, Card card, Color background) {
+		JTextField cardField = new JTextField(card.getCardName());
+		cardField.setEditable(false);
+		cardField.setBackground(background);
+		cardField.setToolTipText(card.getType().toString());
+		panel.add(cardField);
 	}
 }
